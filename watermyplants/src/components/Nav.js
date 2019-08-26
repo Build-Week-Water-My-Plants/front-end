ba8429ff1d7d61f0ba0cefc98d9283e9eb67d629
Kate Efimova <kate.efimova0@gmail.com>
2019-08-26 14:14:24 -0700

    Added navigation, still working on it. Pulling data from master

---
+++ watermyplants/src/components/Nav.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+// import "../sass/nav.scss";
+
+function Nav(props) {
+  return (
+    <nav>
+      <NavLink to="/" activeClassName="activeNavButton" className="logoName">
+        <img src="images/spa-solid.svg" alt="A lotus flower logo" />
+        <h2>Water My Plants</h2>
+      </NavLink>
+      <ul className="nav-links" id="nav-links">
+        <li>
+          <NavLink to="/" activeClassName="activeNavButton">
+            Plants
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/" activeClassName="activeNavButton">
+            Profile
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/" activeClassName="activeNavButton">
+            Sign Out
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
