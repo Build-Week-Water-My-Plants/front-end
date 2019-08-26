eb6672a13bf381103ab4036b674e61bc1f172e48
Brianna Keune <briannakeune@gmail.com>
2019-08-26 17:33:27 -0500

    FirstPlant needs Gigis approval how she wants it to function

--- watermyplants/src/components/FirstPlant.js
+++ watermyplants/src/components/FirstPlant.js
@@ -3,7 +3,14 @@ import React from "react";
 const FirstPlant = props => {
     return(
         <div>
-            
+            <h2>Add Your First Plant!</h2>
+
+            <div>
+                {/* 
+                Link to--PlantForm.js 
+                contains the + svg and leaf svg
+                */}
+            </div>
         </div>
     );
 }
