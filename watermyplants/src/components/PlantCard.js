48832eef955029306095c50361d27e70cdc74d47
Brianna Keune <briannakeune@gmail.com>
2019-08-27 15:12:44 -0500

    added plant card to populate dynamic data for PlantList

--- watermyplants/src/components/PlantCard.js
+++ watermyplants/src/components/PlantCard.js
@@ -1,2 +1,15 @@
 //create card of plant inside plant list of the design file
 //will map over array in plant list and push to card.
+import React from "react";
+
+const PlantCard = props => {
+    return(
+        <div key={props.id}>
+            <h3>{props.name}</h3>
+            <h5>{props.species}</h5>
+            <img src="" alt="Calendar picture" />
+        </div>
+    );
+}
+
+export default PlantCard;
--- watermyplants/src/components/PlantList.js
+++ watermyplants/src/components/PlantList.js
@@ -1,5 +1,6 @@
 //Kate
 import React from "react";
+import PlantCard from "./PlantCard.js";
 
 // prop.arry
 const PlantList = props => {
@@ -7,12 +8,7 @@ const PlantList = props => {
     <section>
       <div class="plant-summary-component">
         {/* Some map function goes here */}
-
-        {/* This is a component, call the component for EACH array object inside `props.array` */}
-        <h3>Nickname</h3>
-        <h5>Species</h5>
-        <img src="" alt="Calendar picture" />
-        {/* ****************************************888 */}
+        
       </div>
       <img src="" alt="Add more plants" />
     </section>
