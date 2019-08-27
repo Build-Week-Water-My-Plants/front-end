6b68193dada551bc1f70fbfc25ba405cf95820e7
Brianna Keune <briannakeune@gmail.com>
2019-08-27 12:29:10 -0500

    and a triple check because I still missed some stuff

--- watermyplants/src/components/ProfileSettings.js
+++ watermyplants/src/components/ProfileSettings.js
@@ -7,12 +7,20 @@ const ProfileSettings = props => {
       <h2>Profile Information</h2>
       <form>
         <label>Username</label>
-        <input type="text" name="username" placeholder="Example" />
+        <input 
+          type="text" 
+          name="username" 
+          placeholder="Example" 
+          />
         <button type="submit">edit</button>
         <label>Phone</label>
-        <input type="tel" name="phone" placeholder="000-000-0000" />
+        <input 
+          type="tel" 
+          name="phone" 
+          placeholder="000-000-0000" 
+          />
         <button type="submit">edit</button>
-        <img src="image/profileSettings.png" alt="slfjsd" />
+        <img src="image/profileSettings.png" alt="hold" />
         {/* quote goes above this img-- 
         "Where flowers bloom so does hope." - Lady Bird Johnson */}
       </form>
