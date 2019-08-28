29ea78e91db02289893996e8562c06c809dc9f73
Brianna Keune <briannakeune@gmail.com>
2019-08-28 12:39:42 -0500

    added FirstPlant ProfileSettings and RecoverPassword routes

--- watermyplants/src/App.js
+++ watermyplants/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.scss";
 import { Route } from "react-router-dom";
-import { PrivateRoute } from "./utils/PrivateRoute";
+import PrivateRoute from "./utils/PrivateRoute";
 
 import { PlantContext } from "./contexts/PlantContexts";
 
@@ -22,9 +22,15 @@ function App() {
   return (
     <div className="App">
       <Nav />
+
       <Route path="/login" render={props => <Login {...props} />} />
       <Route path="/SignUp" render={props => <SignUp {...props} />} />
+      <PrivateRoute path="/plantList" render={props => <PlantList {...props}/>} />
+      <PrivateRoute path="/firstPlant" render={props => <FirstPlant {...props}/>} />
+      <PrivateRoute path="/profile" render={props => <ProfileSettings {...props}/>} />
 
+      {/* this page shouldn't be functional */}
+      <PrivateRoute path="/recoverPassword" render={props => <RecoverPassword {...props}/>} />
 
       {/* Sends an array of objects as dunny data */}
       <Route path="/plant" component={PlantList} />
