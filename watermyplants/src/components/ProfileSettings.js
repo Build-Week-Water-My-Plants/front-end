acd129d6dd1ccca2ba3df3f097bb12c029d56642
Brianna Keune <briannakeune@gmail.com>
2019-08-29 18:04:31 -0500

    state and form should be set for user editing

--- watermyplants/src/components/ProfileSettings.js
+++ watermyplants/src/components/ProfileSettings.js
@@ -1,25 +1,32 @@
 //Bri
 import React, { useState } from "react";
 import Nav from "./Nav";
+import "../sass/ProfileSettings.scss";
 
-import "../sass/ProfileSettings.scss"
+const data = { username: "brikeune", phonenumber: "414-544-6567" };
 const ProfileSettings = props => {
-  const [user, setUser] = useState({username: "", phonenumber: "",})
+  const [username, setUsername] = useState(data.username);
+  const [phonenumber, setPhonenumber] = useState(data.phonenumber);
 
   const profileChangeHandler = e => {
-    console.log(e.target.value);
+    // console.log(e.target.value);
     const name = e.target.name;
     const value = e.target.value;
-    setUser({...user, [name]: value})
-  }
+    setUsername({ ...username, [name]: value });
+    setPhonenumber({ ...phonenumber, [name]: value });
+  };
 
   const submitForm = e => {
     e.preventDefault();
-    console.log(user);
-    props.addNewUser(...user);
-    setUser({
-      username: "", 
-      phonenumber: "",
+    console.log(username);
+    console.log(phonenumber);
+    props.addNewUsername(...username);
+    setUsername({
+      username: ""
+    });
+    props.addNewPhone(...phonenumber);
+    setPhonenumber({
+      phonenumber: ""
     });
   };
   return (
@@ -27,34 +34,44 @@ const ProfileSettings = props => {
       <Nav />
       <section className="profile">
         <h2>Profile Information</h2>
-        <hr/>
+        <hr />
         <form onSubmit={submitForm}>
           <div className="username">
-          <label>Username</label>
-          <input 
-            type="text" 
-            name="username"
-            onChange={profileChangeHandler}
-            value={user.username} 
-            placeholder="Example"
-             />
-          <button type="submit" className="edit">edit</button>
-          </div>
-          <div className="phonenumber">
-          <label>Phone</label>
-          <input 
-            type="tel" 
-            name="phonenumber"
-            onChange={profileChangeHandler}
-            value={user.phonenumber}  
-            placeholder="000-000-0000" 
+            <label>Username</label>
+            <input
+              type="text"
+              name="username"
+              onChange={profileChangeHandler}
+              value={username.username}
+              placeholder="Example"
             />
-          <button type="submit" className="edit">edit</button>
+            <button type="submit" className="edit">
+              edit
+            </button>
           </div>
-          <img src="images/profileSettings.png" alt="persons hands holding some kind of plant in soil." />
+        </form>
+        <form onSubmit={submitForm}>
+          <div className="phonenumber">
+            <label>Phone</label>
+            <input
+              type="tel"
+              name="phonenumber"
+              onChange={profileChangeHandler}
+              value={phonenumber.phonenumber}
+              placeholder="000-000-0000"
+            />
+            <button type="submit" className="edit">
+              edit
+            </button>
+          </div>
+
           {/* quote goes above this img-- 
         "Where flowers bloom so does hope." - Lady Bird Johnson */}
         </form>
+        <img
+          src="images/profileSettings.png"
+          alt="persons hands holding some kind of plant in soil."
+        />
       </section>
     </>
   );
