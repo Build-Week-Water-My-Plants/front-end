e0a412fad1be69bb6f8546e5a1282ac1dd176404
Kate Efimova <kate.efimova0@gmail.com>
2019-08-29 07:58:57 -0700

    Small changes

--- watermyplants/src/components/CreatePlant.js
+++ watermyplants/src/components/CreatePlant.js
@@ -1,9 +1,15 @@
 //Kate
 import React from "react";
 import PlantForm from "./PlantForm";
+import Nav from "./Nav";
 
 const CreatePlant = props => {
-  return <PlantForm />;
+  return (
+    <>
+      <Nav />
+      <PlantForm />
+    </>
+  );
 };
 
 export default CreatePlant;
