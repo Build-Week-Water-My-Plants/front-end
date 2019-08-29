55d18c8e897047673784aedff8517bf7b0a4fd58
Brianna Keune <briannakeune@gmail.com>
2019-08-29 13:49:34 -0500

    added images needed in FirstPlant

--- watermyplants/src/components/FirstPlant.js
+++ watermyplants/src/components/FirstPlant.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Nav from "./Nav";
 
 const FirstPlant = props => {
@@ -6,6 +7,10 @@ const FirstPlant = props => {
     <div>
       <Nav />
       <h2>Add Your First Plant!</h2>
+      <Link to="/addplant">
+        <img src="images/bigAdd.svg" alt="Big Add icon from Font Awesome." className="bigAdd"/>
+      </Link>
+      <img src="images/bigLeaf.svg" alt="Large Leaf icon from Font Awesome." className="bigLeaf" />
     </div>
   );
 };
