c8010e82ce0c142ec62dfaf4c0537b07705af634
Brianna Keune <briannakeune@gmail.com>
2019-08-29 16:02:37 -0500

    forgot to save something

--- watermyplants/src/components/ProfileSettings.js
+++ watermyplants/src/components/ProfileSettings.js
@@ -38,7 +38,6 @@ const ProfileSettings = props => {
             value={user.username} 
             placeholder="Example"
              />
-             <hr/>
           <button type="submit" className="edit">edit</button>
           </div>
           <div className="phonenumber">
@@ -50,7 +49,6 @@ const ProfileSettings = props => {
             value={user.phonenumber}  
             placeholder="000-000-0000" 
             />
-            <hr/>
           <button type="submit" className="edit">edit</button>
           </div>
           <img src="images/profileSettings.png" alt="hold" />
