0f836b1462160a74b3b0fde9851ab92d13d1f2c9
Brianna Keune <briannakeune@gmail.com>
2019-08-29 14:42:04 -0500

    FirstPlant styled

--- watermyplants/src/components/FirstPlant.js
+++ watermyplants/src/components/FirstPlant.js
@@ -2,16 +2,29 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Nav from "./Nav";
 
+import "../sass/FirstPlant.scss";
 const FirstPlant = props => {
   return (
-    <div>
+    <>
       <Nav />
-      <h2>Add Your First Plant!</h2>
-      <Link to="/addplant">
-        <img src="images/bigAdd.svg" alt="Big Add icon from Font Awesome." className="bigAdd"/>
-      </Link>
-      <img src="images/bigLeaf.svg" alt="Large Leaf icon from Font Awesome." className="bigLeaf" />
-    </div>
+      <div className="first-plant">
+        <h2>Add Your First Plant!</h2>
+        <div className="link-images">
+          <Link to="/addplant">
+            <img
+              src="images/bigAdd.svg"
+              alt="Big Add icon from Font Awesome."
+              className="bigAdd"
+            />
+          </Link>
+          <img
+            src="images/bigLeaf.svg"
+            alt="Large Leaf icon from Font Awesome."
+            className="bigLeaf"
+          />
+        </div>
+      </div>
+    </>
   );
 };
 
